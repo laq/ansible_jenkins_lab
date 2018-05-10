a564995b54993ad9c90838c55ec5b5edbabf24ec
Leonardo Quiñonez <leonardoquinonez@seven4n.com>
2018-05-10 13:53:35 -0500

    Tratando crear job que llama a jenkins

--- comandos.sh
+++ comandos.sh
@@ -1,5 +1,15 @@
 #! /bin/bash
 
+################ Requirements ########################
+xdg-open https://www.vagrantup.com/downloads.html
+
+xdg-open https://www.virtualbox.org/wiki/Linux_Downloads
+# apt-get install virtualbox
+
+xdg-open http://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html
+
+
+################ Commands ########################
 # vagrant
 cd vagrant
 vagrant up
@@ -16,8 +26,17 @@ xdg-open http://10.0.0.5
 ansible-playbook -i inventory jenkins_install.yml
 xdg-open http://10.0.0.4:8080
 # configuración de jenkins manual (secret, plugins, admin pass)
+# vagrant ssh cicd
+# sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+# Manual plugins, none
+
+
+
 
 ansible-playbook -i inventory jenkins_job_create.yml
-ansible-playbook -i inventory jenkins_job_invoke.yml
+xdg-open http://10.0.0.4:8080
 
-xdg-open http://10.0.0.5
+
+ansible-playbook -i inventory jenkins_job_invoke.yml
+xdg-open http://10.0.0.5/master/demo.html
+
